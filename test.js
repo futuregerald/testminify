a42adea20b6ab1e0b5792edf81db1277ea902807
Gerald Onyango <gerald@Geralds-MacBook-Pro.local>
2017-07-07 16:45:07 -0400

    doing this

---
+++ test.js
@@ -0,0 +1,17 @@
+console.log('just seeing if this file is minified');
+// Let's see what happens to these comments
+// Let's see what happens to these comments
+// Let's see what happens to these comments
+// Let's see what happens to these comments
+// Let's see what happens to these comments
+
+const name = "G-Money"
+
+
+document.write(`ok I added some extra spaces ${name}`);
+
+
+// more extra space and useless comments
+
+
+                        console.log('alright it is done!');
